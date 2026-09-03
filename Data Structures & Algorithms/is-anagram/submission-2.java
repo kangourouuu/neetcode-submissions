43bce77e41056609fa8151813d6ca482dced9675
kangourouuu <149856695+kangourouuu@users.noreply.github.com>
2026-09-03 18:19:26 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        
+        for (char c : s1) {
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c, 1);
+            }
+        }
+
+        for (char c : t1) {
+            if (map2.containsKey(c)) {
+                map2.put(c, map2.get(c) + 1);
+            } else {
+                map2.put(c, 1);
+            }
+        }
+
+        return map1.equals(map2);
+    }
+}
